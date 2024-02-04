56083c85faa3f76878191cbaf7792ab9938b9f3a
Chris Carbonell <ccarbonell228@gmail.com>
2024-02-04 13:31:22 -0600

    feature: create tables for soruce data in init

--- db/init/init_db.sh
+++ db/init/init_db.sh
@@ -15,3 +15,27 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     $1 
 EOSQL
 }
+
+# create table for specific view from API
+function create_table_for_view {
+    execute """
+        CREATE TABLE IF NOT EXISTS a_raw.${1} (
+            ${1}_id SERIAL,
+            ts_load TIMESTAMP,
+            request_url VARCHAR,
+            status_code INT,
+            res JSONB,
+            PRIMARY KEY (${1}_id, ts_load)
+        )
+        """
+}
+
+# create raw schema
+execute "CREATE SCHEMA a_raw"
+
+# create tables for views
+declare -a views=("team" "roster" "matchup" "settings" "standings")
+for i in "${views[@]}"
+do
+   create_table_for_view "$i"
+done
