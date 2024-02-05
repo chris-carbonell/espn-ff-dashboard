949721bacddd769a2999d3ed174ef147b570f914
Chris Carbonell <ccarbonell228@gmail.com>
2024-02-04 21:18:08 -0600

    feature: extract and load data first then transform via dbt run

--- dbt/cron/update.sh
+++ dbt/cron/update.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+# constants
 UPDATE=false
+
+# update
 if $UPDATE ; then
+    # update data
+    python /home/update/extract_and_load/extract_and_load.py
+
+    # dbt
     dbt run
 fi
