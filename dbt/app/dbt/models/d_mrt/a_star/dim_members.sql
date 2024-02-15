10693b0b0c9b2cb41cc9e19b5e9a5d3dac3c22f2
Chris Carbonell <ccarbonell228@gmail.com>
2024-02-14 21:39:28 -0600

    feature: owners dim

---
+++ dbt/app/dbt/models/d_mrt/a_star/dim_members.sql
@@ -0,0 +1,29 @@
+WITH
+
+	hilaw AS (
+		SELECT * FROM {{ ref('stg_team__members') }}
+	)
+
+    -- dim
+    , dim AS (
+        SELECT DISTINCT
+            {{ dbt_utils.generate_surrogate_key(['season_id', 'scoring_period_id', 'member_id']) }} as member_key
+
+			-- info
+			, season_id
+			, scoring_period_id
+			, member_id
+
+            -- member
+            , member_display_name
+            , member_first_name
+            , member_last_name
+
+        FROM hilaw
+    )
+
+    , lutu AS (
+		SELECT * FROM dim
+	)
+	
+SELECT * FROM lutu
--- dbt/app/dbt/models/d_mrt/a_star/fct_points.sql
+++ dbt/app/dbt/models/d_mrt/a_star/fct_points.sql
@@ -1,22 +1,42 @@
 WITH
 
-	hilaw AS (
+	player_stats AS (
 		SELECT * FROM {{ ref('int_player_stats__clean') }}
+	)
+
+	-- team_owners
+	-- bridge between team and owners (members):
+	-- members.member_id = teams.team_owner_member_id
+	-- stats.team_id = teams.team_id
+	, team_owners AS (
+		SELECT DISTINCT
+			t.season_id
+			, t.scoring_period_id
+			, t.team_id
+			, t.team_owner_member_id
+		FROM {{ ref('int_teams__clean') }} t
 	)
 
 	-- fct_points
 	, fct_points AS (
 		SELECT
 			-- surrogate keys
-			{{ dbt_utils.generate_surrogate_key(['team_id']) }} as team_key
-			, {{ dbt_utils.generate_surrogate_key(['player_id']) }} as player_key
-			, {{ dbt_utils.generate_surrogate_key(['stat']) }} as stat_key
+			{{ dbt_utils.generate_surrogate_key(['ps.season_id', 'ps.scoring_period_id', 'ps.stat_id']) }} as stat_key
+			, {{ dbt_utils.generate_surrogate_key(['ps.season_id', 'ps.scoring_period_id']) }} as time_key
+			, {{ dbt_utils.generate_surrogate_key(['ps.season_id', 'ps.scoring_period_id', 'ps.player_id']) }} as player_key
+			, {{ dbt_utils.generate_surrogate_key(['ps.season_id', 'ps.scoring_period_id', 'ps.team_id']) }} as team_key
+			, {{ dbt_utils.generate_surrogate_key(['ps.season_id', 'ps.scoring_period_id', 't.team_owner_member_id']) }} as member_key
 
 			-- facts
-			, points_projected
-			, points_actual
+			, ps.points_projected
+			, ps.points_actual
 				
-		FROM hilaw
+		FROM player_stats ps
+
+		LEFT JOIN team_owners t
+		ON ps.season_id = t.season_id
+			AND ps.scoring_period_id = t.scoring_period_id
+			AND ps.team_id = t.team_id
 	)
 
 	, lutu AS (
