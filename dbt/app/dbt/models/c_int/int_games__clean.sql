68ce55ff2ef5c84c0b3a78f74eb2428b1af20e98
Chris Carbonell <ccarbonell228@gmail.com>
2024-02-18 22:07:30 -0600

    fix: deduplicate games (one for home, one for away)

--- dbt/app/dbt/models/c_int/int_games__clean.sql
+++ dbt/app/dbt/models/c_int/int_games__clean.sql
@@ -4,21 +4,19 @@ WITH
 		SELECT * FROM {{ ref('stg_pro_team_schedules__games') }}
 	)
 	
+	-- games
+	-- the scrape duplicates records
+	-- because the schedule is scraped for the home team and the away team
+	-- so use DISTINCT to deduplicate them
 	, games AS (
-		SELECT
-			request_id
-			
+		SELECT DISTINCT
+			CAST(game_id AS BIGINT) AS game_id
+
 			, CAST(season_id AS INTEGER) AS season_id
-			
-			, CAST(pro_team_id AS INTEGER) AS pro_team_id
-			, pro_team_name
-			, pro_team_abbrev
-			, pro_team_location
-			, pro_team_bye_week
-			
 			, CAST(scoring_period_id AS INTEGER) AS scoring_period_id
-			, game_id
+			
 			, TIMEZONE('America/Chicago', TO_TIMESTAMP(LEFT(epoch_game, -3):: BIGINT)) AS ts_game
+
 			, CAST(pro_team_id_home AS INTEGER) AS pro_team_id_home
 			, CAST(pro_team_id_away AS INTEGER) AS pro_team_id_away
 			
